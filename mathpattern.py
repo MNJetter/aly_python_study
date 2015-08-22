c0377ba2718debd25843be711a1fcc396a25b3d4
MNJetter <mnjetter@gmail.com>
2015-08-23 02:38:09 +0900

    Create mathpattern.py

---
+++ mathpattern.py
@@ -0,0 +1,10 @@
+### Built for Python 3.4 -- No guarantees with other versions because I don't know enough about it yet. ###
+
+number1 = int(input('Gimme a number: '))
+number2 = int(input('Now gimme another number: '))
+
+difference = number1 - number2
+addition = number1 + number2
+
+print('Your result is...')
+print("%d%d"%(difference,addition))
