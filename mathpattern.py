dc483132b5fb7eecb128733cd03d70751bdd3a35
MNJetter <mnjetter@gmail.com>
2015-08-23 12:24:33 +0900

    Update mathpattern.py

--- mathpattern.py
+++ mathpattern.py
@@ -1,10 +1,10 @@
 ### Built for Python 3.4 -- No guarantees with other versions because I don't know enough about it yet. ###
 
-number1 = int(input('Gimme a number: '))
-number2 = int(input('Now gimme another number: '))
+num1 = int(input('Gimme a number: '))
+num2 = int(input('Now gimme another number: '))
 
-difference = number1 - number2
-addition = number1 + number2
+minusnums = num1 - num2
+plusnums = num1 + num2
 
 print('Your result is...')
-print("%d%d"%(difference,addition))
+print("%d%d"%(minusnums,plusnums))
