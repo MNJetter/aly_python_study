94cdbefcb730417e3cd671ddbf0de632074b3789
MNJetter <mnjetter@gmail.com>
2015-08-24 13:27:03 +0900

    Update LPTHW_ex3_main.py
    Added comments as per Study Drill 1.

--- LPTHW_ex3_main.py
+++ LPTHW_ex3_main.py
@@ -1,23 +1,37 @@
+# Writes a statement.
 print "I will now count my chickens:"
 
+# Writes a statement followed by the restuls of a math equation.
 print "Hens", 25 + 30 / 6
+# Writes a statement followed by the results of a math equation.
 print "Roosters", 100 - 25 * 3 % 4
 
+# Writes a statement.
 print "Now I will count the eggs:"
 
+# Writes the results of a math equation.
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Writes a question.
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# Calculates the answer to that question (t/f).
 print 3 + 2 < 5 - 7
 
+# Prints a question and the results of a math equation.
 print "What is 3 + 2?", 3 + 2
+# Prints a question and the results of a math equation.
 print "What is 5 - 7?", 5 - 7
 
+# Prints a statement.
 print "Oh, that's why it's False."
 
+# Prints a statement.
 print "How about some more."
 
+# Prints a question and calculates the answer (t/f).
 print "Is it greater?", 5 > -2
+# Prints a question and calculates the answer (t/f).
 print "Is it greater or equal?", 5 >= -2
+# Prints a question and calculates the answer (t/f).
 print "Is it less or equal?", 5 <= -2
