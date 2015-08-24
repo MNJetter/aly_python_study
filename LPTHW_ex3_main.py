4ad6f4991b588b2724f7ae4fb663ca0c5436d0ce
MNJetter <mnjetter@gmail.com>
2015-08-24 13:29:27 +0900

    Update LPTHW_ex3_main.py
    Added floating points as per Study Drill 5.

--- LPTHW_ex3_main.py
+++ LPTHW_ex3_main.py
@@ -2,26 +2,26 @@
 print "I will now count my chickens:"
 
 # Writes a statement followed by the restuls of a math equation.
-print "Hens", 25 + 30 / 6
+print "Hens", 25.00 + 30.00 / 6.00
 # Writes a statement followed by the results of a math equation.
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.00 - 25.00 * 3.00 % 4.00
 
 # Writes a statement.
 print "Now I will count the eggs:"
 
 # Writes the results of a math equation.
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
 
 # Writes a question.
 print "Is it true that 3 + 2 < 5 - 7?"
 
 # Calculates the answer to that question (t/f).
-print 3 + 2 < 5 - 7
+print 3.00 + 2.00 < 5.00 - 7.00
 
 # Prints a question and the results of a math equation.
-print "What is 3 + 2?", 3 + 2
+print "What is 3 + 2?", 3.00 + 2.00
 # Prints a question and the results of a math equation.
-print "What is 5 - 7?", 5 - 7
+print "What is 5 - 7?", 5.00 - 7.00
 
 # Prints a statement.
 print "Oh, that's why it's False."
@@ -30,8 +30,8 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # Prints a question and calculates the answer (t/f).
-print "Is it greater?", 5 > -2
+print "Is it greater?", 5.00 > -2.00
 # Prints a question and calculates the answer (t/f).
-print "Is it greater or equal?", 5 >= -2
+print "Is it greater or equal?", 5.00 >= -2.00
 # Prints a question and calculates the answer (t/f).
-print "Is it less or equal?", 5 <= -2
+print "Is it less or equal?", 5.00 <= -2.00
